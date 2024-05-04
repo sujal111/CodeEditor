fbbba417be8c57aae43bea829638ad4391eb7e0a
sujalmaiti <sujalmaiti123456@gmail.com>
2024-05-04 19:53:06 +0530

    problem-desc

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import ProblemDescription from './pages/Description/ProblemDescription'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    Hello World
+    <ProblemDescription/>
     </>
   )
 }
